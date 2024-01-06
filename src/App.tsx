641d904c4c0901000829f88aa2402f7ff5c909ed
souvikotaku <manson1989@>
2024-01-06 17:51:52 +0530

    added files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+let name: string;
+let age: number;
+
+//union example
+let age2: number | string;
+
+age2 = "33 years";
+
+function printName(name: string) {
+  console.log(name);
+}
+
+let isStudent: boolean;
+let hobbies: string[];
+
+//tuple example of typescript
+let role: [string, number];
+role = ["Student", 3];
+
+//object example of typescript
+
+type Person = {
+  name: string;
+  age: number;
+};
+let person: Person = {
+  name: "Souvik",
+  age: 33,
+};
+
+//interface example of typescript
+
+interface Person2 extends Person {
+  profession: string;
+}
+
+let newObj: Person2 = {
+  name: "Souvik",
+  age: 33,
+  profession: "plastic surgeon",
+};
+
+console.log(newObj);
+
+name = "Souvik";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
 
 export default App;
